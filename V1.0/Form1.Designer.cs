7c935d2fb39f8b7abc57572ee1027202290c1a92
ucjpriyo <ptestingsoftware59@gmail.com>
2025-05-22 11:41:29 +0530

    Initial commit

---
+++ V1.0/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace V1._0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtTask = new System.Windows.Forms.TextBox();
+            this.lblTask = new System.Windows.Forms.Label();
+            this.txtDueDate = new System.Windows.Forms.TextBox();
+            this.lblDueDate = new System.Windows.Forms.Label();
+            this.listBoxTasks = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(111, 131);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 43);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "ADD";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtTask
+            // 
+            this.txtTask.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTask.Location = new System.Drawing.Point(111, 41);
+            this.txtTask.Name = "txtTask";
+            this.txtTask.Size = new System.Drawing.Size(139, 30);
+            this.txtTask.TabIndex = 1;
+            // 
+            // lblTask
+            // 
+            this.lblTask.AutoSize = true;
+            this.lblTask.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTask.Location = new System.Drawing.Point(12, 44);
+            this.lblTask.Name = "lblTask";
+            this.lblTask.Size = new System.Drawing.Size(56, 25);
+            this.lblTask.TabIndex = 2;
+            this.lblTask.Text = "Task";
+            // 
+            // txtDueDate
+            // 
+            this.txtDueDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDueDate.Location = new System.Drawing.Point(111, 77);
+            this.txtDueDate.Name = "txtDueDate";
+            this.txtDueDate.Size = new System.Drawing.Size(139, 30);
+            this.txtDueDate.TabIndex = 1;
+            // 
+            // lblDueDate
+            // 
+            this.lblDueDate.AutoSize = true;
+            this.lblDueDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDueDate.Location = new System.Drawing.Point(12, 80);
+            this.lblDueDate.Name = "lblDueDate";
+            this.lblDueDate.Size = new System.Drawing.Size(94, 25);
+            this.lblDueDate.TabIndex = 2;
+            this.lblDueDate.Text = "Due Date";
+            // 
+            // listBoxTasks
+            // 
+            this.listBoxTasks.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBoxTasks.FormattingEnabled = true;
+            this.listBoxTasks.ItemHeight = 20;
+            this.listBoxTasks.Location = new System.Drawing.Point(256, 44);
+            this.listBoxTasks.Name = "listBoxTasks";
+            this.listBoxTasks.Size = new System.Drawing.Size(250, 124);
+            this.listBoxTasks.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(518, 218);
+            this.Controls.Add(this.listBoxTasks);
+            this.Controls.Add(this.lblDueDate);
+            this.Controls.Add(this.lblTask);
+            this.Controls.Add(this.txtDueDate);
+            this.Controls.Add(this.txtTask);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtTask;
+        private System.Windows.Forms.Label lblTask;
+        private System.Windows.Forms.TextBox txtDueDate;
+        private System.Windows.Forms.Label lblDueDate;
+        private System.Windows.Forms.ListBox listBoxTasks;
+    }
+}
+
---
+++ V1.0/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace V1._0
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string taskName = txtTask.Text;
+            string dueDate = txtDueDate.Text;
+
+            // No validation in v1.0
+            string taskEntry = taskName + " - Due: " + dueDate;
+            listBoxTasks.Items.Add(taskEntry);
+
+            // Clear inputs
+            txtTask.Clear();
+            txtDueDate.Clear();
+        }
+
+    }
+}
